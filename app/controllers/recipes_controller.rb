fdea6c15ca2d1322d0c2d812474c4bf04e64790e
asell22 <alexanderjsell@gmail.com>
2015-07-13 12:24:29 -0700

    'Creating Recipes' feature complete

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,4 +5,24 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
   end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      flash[:notice] = "Recipe has been added"
+      redirect_to @recipe
+    else
+    end
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :servings, :cooking_time, :level_of_difficulty, :ingredients, :procedure)
+  end
 end
--- spec/features/creating_recipes_spec.rb
+++ spec/features/creating_recipes_spec.rb
@@ -9,12 +9,12 @@ feature 'Creating Recipes' do
     fill_in 'Name', with: "Bowl of Cereal"
     fill_in 'Ingredients', with: "Milk Fruitloops"
     fill_in 'Procedure', with: "Pour cereal into bowl, pour 3/4 cup of milk over it"
-    select 1, from: 'Servings'
-    select '1-5 minutes', from: 'Cooking Time'
-    select 'Simple', from: 'Level of Difficulty'
+    select 1, from: :servings
+    select '1-5 minutes', from: :cooking_time
+    select 'Simple', from: :level_of_difficulty
 
     click_button 'Add Recipe'
 
-    expect(page).to have_content('Recipe has been created.')
+    expect(page).to have_content('Recipe has been added')
   end
 end
