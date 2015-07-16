50837c44f8d7acb507d02850ae4193c4ad0bc699
asell22 <alexanderjsell@gmail.com>
2015-07-15 17:13:45 -0700

    Seeded FoodType FoodPreference Cuisine models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-FoodPreference.create([{name: 'Dairy Free', 'Vegan', 'Vegetarian', 'Meat', 'Other'}])
+# FoodPreference.create([{name: 'Dairy Free'}, {name: 'Vegan'}, {name: 'Vegetarian'}, {name: 'Meat'}, {name: 'Other'}])
+
+FoodType.create([{name: 'Main Course'}, {name: 'Side Dish'}, {name: 'Dessert'}, {name: 'Breakfast'}, {name: 'Beverage'}, {name: 'Other'}])
+
+Cuisine.create([{name: 'French'}, {name: 'Italian'}, {name: 'Chinese'}, {name: 'Indian'}, {name: 'Thai'}, {name: 'Mexican'},{name: 'Japanese'}, {name: 'Spanish'}, {name: 'Greek'}, {name: 'Lebanese'}, {name: 'American'}, {name: 'Other'}])
