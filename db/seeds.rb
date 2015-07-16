bda0dcc61ea1f51958501e29d124b52bdffdaffd
asell22 <alexanderjsell@gmail.com>
2015-07-15 18:19:25 -0700

    Placeholders for all select boxes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-# FoodPreference.create([{name: 'Dairy Free'}, {name: 'Vegan'}, {name: 'Vegetarian'}, {name: 'Meat'}, {name: 'Other'}])
+
+FoodPreference.create([{name: 'Dairy Free'}, {name: 'Vegan'}, {name: 'Vegetarian'}, {name: 'Meat'}, {name: 'Other'}])
 
 FoodType.create([{name: 'Main Course'}, {name: 'Side Dish'}, {name: 'Dessert'}, {name: 'Breakfast'}, {name: 'Beverage'}, {name: 'Other'}])
 
-Cuisine.create([{name: 'French'}, {name: 'Italian'}, {name: 'Chinese'}, {name: 'Indian'}, {name: 'Thai'}, {name: 'Mexican'},{name: 'Japanese'}, {name: 'Spanish'}, {name: 'Greek'}, {name: 'Lebanese'}, {name: 'American'}, {name: 'Other'}])
+ Cuisine.create([{name: 'French'}, {name: 'Italian'}, {name: 'Chinese'}, {name: 'Indian'}, {name: 'Thai'}, {name: 'Mexican'},{name: 'Japanese'}, {name: 'Spanish'}, {name: 'Greek'}, {name: 'Lebanese'}, {name: 'American'}, {name: 'Other'}])
