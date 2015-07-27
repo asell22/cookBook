7e124bea3474674cb14c70ab6db2633dd4a9440e
asell22 <alexanderjsell@gmail.com>
2015-07-27 16:07:36 -0700

    Added body column to comments table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150727224449) do
+ActiveRecord::Schema.define(version: 20150727230627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150727224449) do
     t.integer  "recipe_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "body"
   end
 
   add_index "comments", ["recipe_id"], name: "index_comments_on_recipe_id", using: :btree
